d961a5b7c3aa57741ee3f550bcbc2be67caf0ca0
SomnumExterreri <90258830+SomnumExterreri@users.noreply.github.com>
2023-03-01 18:28:02 +0100

    Help

--- zaverecnaPrace/SqlRepository.cs
+++ zaverecnaPrace/SqlRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,35 @@ namespace zaverecnaPrace
 {
     internal class SqlRepository
     {
+        public string Connection { get; set; }
+        byte[] passwordSalt;
+        byte[] passwordHash;
 
+
+        public SqlRepository(string connection)
+        {
+            this.Connection = connection;
+        }
+
+        public void Login(string Username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(Connection))
+            {
+                connection.Open();
+                using(SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Users where Username = @username";
+                    cmd.Parameters.AddWithValue("username", Username);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if(reader.Read())
+                        {
+
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
     }
 }
---
+++ zaverecnaPrace/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaverecnaPrace
+{
+    internal class User
+    {
+        public User(string userName, byte[] password, byte[] passwordSalt)
+        {
+            UserName = userName;
+            Password = password;
+            PasswordSalt = passwordSalt;
+        }
+
+        public string UserName { get; set; }
+        public byte[] Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
